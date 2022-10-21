a235880801a824f99157128b404e75f7cc94d782
hildelio <hildelio@gmail.com>
2022-10-20 21:12:07 -0300

    Requisito 1 ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Pokedex from './components/Pokedex';
 
-function App() {
-  return (
-    <h1>Exercise - Pokedex!</h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Exercise - Pokedex!</h1>
+        <Pokedex />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Pokedex.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Pokedex extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pokédex</h1>
+        <ul />
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
